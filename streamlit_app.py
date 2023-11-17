95d0ebddfc0333c2030d47a476fba0bfe4f18804
Abdulrahman Aroworamimo <109465283+Abdul-AA@users.noreply.github.com>
2023-11-16 22:03:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # Function to load data (ensure to replace with your actual data path)
 @st.cache
 def load_data():
-    return pd.read_csv('/path/to/your/data.csv', parse_dates=['Crash Date/Time'])
+    return pd.read_csv('Crash_Reporting_-_Drivers_Data.csv', parse_dates=['Crash Date/Time'])
 
 df = load_data()
 
