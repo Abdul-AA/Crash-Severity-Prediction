8bbdf6adee4c3fe7c09273cd04b9807514a8e508
Abdulrahman Aroworamimo <109465283+Abdul-AA@users.noreply.github.com>
2023-11-16 23:45:04 -0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import matplotlib.pyplot as plt
 
-
-# Function to load data (ensure to replace with your actual data path)
+# Function to load data
 @st.cache
 def load_data():
     return pd.read_csv('Crash_Reporting_-_Drivers_Data.csv', parse_dates=['Crash Date/Time'])
@@ -15,19 +13,24 @@ df = load_data()
 st.sidebar.header("Filters")
 severity_filter = st.sidebar.selectbox("Select Injury Severity", ['All'] + list(df['Injury Severity'].unique()))
 
-
 # Filter data based on severity
 if severity_filter != 'All':
     df = df[df['Injury Severity'] == severity_filter]
 
 # Map Visualization
 st.header("Accident Locations on Map")
-plt.figure(figsize=(40,15))
+
+# Custom color scale
+color_scale = {
+    "NO APPARENT INJURY": "green",
+    "FATAL INJURY": "red",
+    "POSSIBLE INJURY": "lightcoral",
+    "SUSPECTED MINOR INJURY": "orange",
+    "SUSPECTED SERIOUS INJURY": "darkred"
+}
+
 map_fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Injury Severity",
                             hover_data=['Route Type', 'Weather', 'Light'],
-                            zoom=10, height=500, mapbox_style="open-street-map")
+                            zoom=10, height=700, mapbox_style="open-street-map",
+                            color_discrete_map=color_scale)
 st.plotly_chart(map_fig)
-
-
-
-
