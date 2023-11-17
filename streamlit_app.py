c390f4d2958c06d408ec982b60ffa5406ac7f7d5
Abdulrahman Aroworamimo <109465283+Abdul-AA@users.noreply.github.com>
2023-11-16 22:14:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 
 # Function to load data (ensure to replace with your actual data path)
