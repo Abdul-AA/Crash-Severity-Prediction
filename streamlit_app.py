21f48f09a888215f903c3a678991c63ee8f24039
Abdulrahman Aroworamimo <109465283+Abdul-AA@users.noreply.github.com>
2023-11-18 07:25:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,63 +37,66 @@ def shap_waterfall_plot(model, input_data):
         st.error("SHAP waterfall plot is only available for individual predictions.")
 
 def main():
-    st.title('Vehicle Accident Injury Severity Prediction: High Emergency or Low Emergency?')
-    st.markdown("""
-        <style>
-        .big-font {
-            font-size:20px !important;
-        }
-        </style>
-        """, unsafe_allow_html=True)
-
-    with st.form("prediction_form"):
-        st.subheader("Check the Following Boxes for YES, Skip for NO:")
-        
-        # Your input fields
-        inputs = {
+    tab1,tab2,tab3=st.tabs(['Make a Prediction','See SHAP Summary Plot','See Waterfall Plot'])
+    with tab1:
+        st.title('Vehicle Accident Injury Severity Prediction: High Emergency or Low Emergency?')
+        st.markdown("""
+            <style>
+            .big-font {
+                font-size:20px !important;
+            }
+            </style>
+            """, unsafe_allow_html=True)
+    
+        with st.form("prediction_form"):
+            st.subheader("Check the Following Boxes for YES, Skip for NO:")
             
-            'Vehicle First Impact Location_SIX OCLOCK': st.checkbox('Did the vehicle first impact location occur at six o\'clock of the Vehicle?'),
-            'Vehicle Damage Extent_NO DAMAGE': st.checkbox('Was there no damage to the vehicle?'),
-            'Vehicle Damage Extent_SUPERFICIAL': st.checkbox('Was the vehicle damage superficial?'),
-            'Vehicle Damage Extent_FUNCTIONAL': st.checkbox('Is the Vehicle still functional?'),
-            'Vehicle Second Impact Location_SIX OCLOCK': st.checkbox('Did the vehicle second impact location occur at six o\'clock of the Vehicle?'),
-            'Vehicle Damage Extent_DISABLING': st.checkbox('Was the vehicle damage disabling?'),
-            'Collision Type_SAME DIRECTION SIDESWIPE': st.checkbox('Was the collision a same direction sideswipe?'),
-            'Vehicle Body Type_MOTORCYCLE': st.checkbox('Is it a motorcycle?'),
-            'Vehicle Movement_BACKING': st.checkbox('Was the vehicle backing?'),
-            'Collision Type_SAME DIR REAR END': st.checkbox('Was the collision a same direction rear end?'),
-            'Collision Type_HEAD ON': st.checkbox('Was the collision head on?'),
-            'Vehicle Movement_MOVING CONSTANT SPEED': st.checkbox('Was the vehicle moving at a constant speed?'),
-            'Vehicle Body Type_SCHOOL BUS': st.checkbox('Is it a school bus?'),
-            'Vehicle Second Impact Location_FOUR OCLOCK': st.checkbox('Did the vehicle second impact location occur at four o\'clock of the Vehicle?'),
-            'Vehicle Body Type_PASSENGER CAR': st.checkbox('Is it a passenger car?'),
-            'Vehicle Second Impact Location_NINE OCLOCK': st.checkbox('Did the vehicle second impact location occur at nine o\'clock of the Vehicle?'),
-            'Collision Type_OTHER': st.checkbox('Check if the collision type was not one of the following: Same Direction Rear End, Same Direction Sideswipe, Head On, Angle, Opposite Direction Sideswipe, Opposite Direction Rear End, Same Movement Angle, Straight Movement Angle'),
-            'Collision Type_STRAIGHT MOVEMENT ANGLE': st.checkbox('Was the collision a straight movement angle?'),
-            'Vehicle First Impact Location_TWELVE OCLOCK': st.checkbox('Did the vehicle first impact location occur at twelve o\'clock of the Vehicle?'),
-            'Vehicle Second Impact Location_TWELVE OCLOCK': st.checkbox('Did the vehicle second impact location occur at twelve o\'clock of the Vehicle?')
-        
- 
-        }
-
-        submit_button = st.form_submit_button("See Level of Emergency")
-
-        if submit_button:
-            input_df = pd.DataFrame([inputs])
-            prediction = predict_injury_severity(input_df)
-
-    # Use HTML with inline CSS for color styling
-            if prediction == 1:
-                severity = '<span style="color: red;">High</span>'
-            else:
-                severity = '<span style="color: green;">Low</span>'
-
-            st.markdown(f'<p class="big-font">Emergency Level: <b>{severity}</b></p>', unsafe_allow_html=True)
+            # Your input fields
+            inputs = {
+                
+                'Vehicle First Impact Location_SIX OCLOCK': st.checkbox('Did the vehicle first impact location occur at six o\'clock of the Vehicle?'),
+                'Vehicle Damage Extent_NO DAMAGE': st.checkbox('Was there no damage to the vehicle?'),
+                'Vehicle Damage Extent_SUPERFICIAL': st.checkbox('Was the vehicle damage superficial?'),
+                'Vehicle Damage Extent_FUNCTIONAL': st.checkbox('Is the Vehicle still functional?'),
+                'Vehicle Second Impact Location_SIX OCLOCK': st.checkbox('Did the vehicle second impact location occur at six o\'clock of the Vehicle?'),
+                'Vehicle Damage Extent_DISABLING': st.checkbox('Was the vehicle damage disabling?'),
+                'Collision Type_SAME DIRECTION SIDESWIPE': st.checkbox('Was the collision a same direction sideswipe?'),
+                'Vehicle Body Type_MOTORCYCLE': st.checkbox('Is it a motorcycle?'),
+                'Vehicle Movement_BACKING': st.checkbox('Was the vehicle backing?'),
+                'Collision Type_SAME DIR REAR END': st.checkbox('Was the collision a same direction rear end?'),
+                'Collision Type_HEAD ON': st.checkbox('Was the collision head on?'),
+                'Vehicle Movement_MOVING CONSTANT SPEED': st.checkbox('Was the vehicle moving at a constant speed?'),
+                'Vehicle Body Type_SCHOOL BUS': st.checkbox('Is it a school bus?'),
+                'Vehicle Second Impact Location_FOUR OCLOCK': st.checkbox('Did the vehicle second impact location occur at four o\'clock of the Vehicle?'),
+                'Vehicle Body Type_PASSENGER CAR': st.checkbox('Is it a passenger car?'),
+                'Vehicle Second Impact Location_NINE OCLOCK': st.checkbox('Did the vehicle second impact location occur at nine o\'clock of the Vehicle?'),
+                'Collision Type_OTHER': st.checkbox('Check if the collision type was not one of the following: Same Direction Rear End, Same Direction Sideswipe, Head On, Angle, Opposite Direction Sideswipe, Opposite Direction Rear End, Same Movement Angle, Straight Movement Angle'),
+                'Collision Type_STRAIGHT MOVEMENT ANGLE': st.checkbox('Was the collision a straight movement angle?'),
+                'Vehicle First Impact Location_TWELVE OCLOCK': st.checkbox('Did the vehicle first impact location occur at twelve o\'clock of the Vehicle?'),
+                'Vehicle Second Impact Location_TWELVE OCLOCK': st.checkbox('Did the vehicle second impact location occur at twelve o\'clock of the Vehicle?')
             
+     
+            }
+    
+            submit_button = st.form_submit_button("See Level of Emergency")
+    
+            if submit_button:
+                input_df = pd.DataFrame([inputs])
+                prediction = predict_injury_severity(input_df)
+    
+        # Use HTML with inline CSS for color styling
+                if prediction == 1:
+                    severity = '<span style="color: red;">High</span>'
+                else:
+                    severity = '<span style="color: green;">Low</span>'
+    
+                st.markdown(f'<p class="big-font">Emergency Level: <b>{severity}</b></p>', unsafe_allow_html=True)
+    with tab2:   
             # Display SHAP summary and waterfall plots
             st.subheader("SHAP Summary Explanation")
             st.write("This plot shows the impact of each feature on the model's output for the prediction.")
             shap_summary_plot(model, input_df)
+    with tab3:
 
             st.subheader("SHAP Waterfall Explanation for the Prediction")
             st.write("This plot shows how each feature contributes to the individual prediction, moving from the base value to the final output.")
