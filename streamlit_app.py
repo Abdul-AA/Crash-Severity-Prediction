78f32b7cf6a9955321cb88389d0dab187f9c5022
Abdulrahman Aroworamimo <109465283+Abdul-AA@users.noreply.github.com>
2023-11-18 07:42:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,39 +50,43 @@ def main():
     
         with st.form("prediction_form"):
             st.subheader("Check the Following Boxes for YES, Skip for NO:")
-            
+            col1,col2=st.columns(2)
+            with col1:
             # Your input fields
-            inputs = {
+                inputs1 = {
+                    
+                    'Vehicle First Impact Location_SIX OCLOCK': st.checkbox('Did the vehicle first impact location occur at six o\'clock of the Vehicle?'),
+                    'Vehicle Damage Extent_NO DAMAGE': st.checkbox('Was there no damage to the vehicle?'),
+                    'Vehicle Damage Extent_SUPERFICIAL': st.checkbox('Was the vehicle damage superficial?'),
+                    'Vehicle Damage Extent_FUNCTIONAL': st.checkbox('Is the Vehicle still functional?'),
+                    'Vehicle Second Impact Location_SIX OCLOCK': st.checkbox('Did the vehicle second impact location occur at six o\'clock of the Vehicle?'),
+                    'Vehicle Damage Extent_DISABLING': st.checkbox('Was the vehicle damage disabling?'),
+                    'Collision Type_SAME DIRECTION SIDESWIPE': st.checkbox('Was the collision a same direction sideswipe?'),
+                    'Vehicle Body Type_MOTORCYCLE': st.checkbox('Is it a motorcycle?'),
+                    'Vehicle Movement_BACKING': st.checkbox('Was the vehicle backing?'),
+                    'Collision Type_SAME DIR REAR END': st.checkbox('Was the collision a same direction rear end?')}
+            with col2:
+                inputs2={
+                    'Collision Type_HEAD ON': st.checkbox('Was the collision head on?'),
+                    'Vehicle Movement_MOVING CONSTANT SPEED': st.checkbox('Was the vehicle moving at a constant speed?'),
+                    'Vehicle Body Type_SCHOOL BUS': st.checkbox('Is it a school bus?'),
+                    'Vehicle Second Impact Location_FOUR OCLOCK': st.checkbox('Did the vehicle second impact location occur at four o\'clock of the Vehicle?'),
+                    'Vehicle Body Type_PASSENGER CAR': st.checkbox('Is it a passenger car?'),
+                    'Vehicle Second Impact Location_NINE OCLOCK': st.checkbox('Did the vehicle second impact location occur at nine o\'clock of the Vehicle?'),
+                    'Collision Type_OTHER': st.checkbox('Check if the collision type was not one of the following: Same Direction Rear End, Same Direction Sideswipe, Head On, Angle, Opposite Direction Sideswipe, Opposite Direction Rear End, Same Movement Angle, Straight Movement Angle'),
+                    'Collision Type_STRAIGHT MOVEMENT ANGLE': st.checkbox('Was the collision a straight movement angle?'),
+                    'Vehicle First Impact Location_TWELVE OCLOCK': st.checkbox('Did the vehicle first impact location occur at twelve o\'clock of the Vehicle?'),
+                    'Vehicle Second Impact Location_TWELVE OCLOCK': st.checkbox('Did the vehicle second impact location occur at twelve o\'clock of the Vehicle?')
                 
-                'Vehicle First Impact Location_SIX OCLOCK': st.checkbox('Did the vehicle first impact location occur at six o\'clock of the Vehicle?'),
-                'Vehicle Damage Extent_NO DAMAGE': st.checkbox('Was there no damage to the vehicle?'),
-                'Vehicle Damage Extent_SUPERFICIAL': st.checkbox('Was the vehicle damage superficial?'),
-                'Vehicle Damage Extent_FUNCTIONAL': st.checkbox('Is the Vehicle still functional?'),
-                'Vehicle Second Impact Location_SIX OCLOCK': st.checkbox('Did the vehicle second impact location occur at six o\'clock of the Vehicle?'),
-                'Vehicle Damage Extent_DISABLING': st.checkbox('Was the vehicle damage disabling?'),
-                'Collision Type_SAME DIRECTION SIDESWIPE': st.checkbox('Was the collision a same direction sideswipe?'),
-                'Vehicle Body Type_MOTORCYCLE': st.checkbox('Is it a motorcycle?'),
-                'Vehicle Movement_BACKING': st.checkbox('Was the vehicle backing?'),
-                'Collision Type_SAME DIR REAR END': st.checkbox('Was the collision a same direction rear end?'),
-                'Collision Type_HEAD ON': st.checkbox('Was the collision head on?'),
-                'Vehicle Movement_MOVING CONSTANT SPEED': st.checkbox('Was the vehicle moving at a constant speed?'),
-                'Vehicle Body Type_SCHOOL BUS': st.checkbox('Is it a school bus?'),
-                'Vehicle Second Impact Location_FOUR OCLOCK': st.checkbox('Did the vehicle second impact location occur at four o\'clock of the Vehicle?'),
-                'Vehicle Body Type_PASSENGER CAR': st.checkbox('Is it a passenger car?'),
-                'Vehicle Second Impact Location_NINE OCLOCK': st.checkbox('Did the vehicle second impact location occur at nine o\'clock of the Vehicle?'),
-                'Collision Type_OTHER': st.checkbox('Check if the collision type was not one of the following: Same Direction Rear End, Same Direction Sideswipe, Head On, Angle, Opposite Direction Sideswipe, Opposite Direction Rear End, Same Movement Angle, Straight Movement Angle'),
-                'Collision Type_STRAIGHT MOVEMENT ANGLE': st.checkbox('Was the collision a straight movement angle?'),
-                'Vehicle First Impact Location_TWELVE OCLOCK': st.checkbox('Did the vehicle first impact location occur at twelve o\'clock of the Vehicle?'),
-                'Vehicle Second Impact Location_TWELVE OCLOCK': st.checkbox('Did the vehicle second impact location occur at twelve o\'clock of the Vehicle?')
-            
-     
-            }
+         
+                }
     
             submit_button = st.form_submit_button("See Level of Emergency")
     
             if submit_button:
-                input_df = pd.DataFrame([inputs])
-                prediction = predict_injury_severity(input_df)
+            inputs = {**inputs_col1, **inputs_col2}  # Combine inputs from both columns
+            input_df = pd.DataFrame([inputs])
+            prediction = predict_injury_severity(input_df)
     
         # Use HTML with inline CSS for color styling
                 if prediction == 1:
